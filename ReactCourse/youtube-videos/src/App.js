9fa2cde35d4f5bdc38af875a78162f93d81a0f6a
aaculp <aaculp@icloud.com>
2020-03-01 22:32:10 -0500

    added working VideoDetail so when you click the video the name generates. Working prop system, gaining a better understanding of destructing

--- ReactCourse/youtube-videos/src/App.js
+++ ReactCourse/youtube-videos/src/App.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 import SearchBar from './components/SearchBar'
 import youtube from './api/youtube'
 import VideoList from './components/VideoList'
+import VideoDetail from './components/VideoDetail'
 
 export default class App extends Component {
-    state = { videos: [], selectedVideo: null }
+    state = { videos: [], selectedVideo: null}
 
     onTermSubmit = async term => {
         // console.log(term)
@@ -20,15 +21,14 @@ export default class App extends Component {
     }
 
     onVideoSelect = (video) => {
-        console.log('from the app', video)
+        this.setState({ selectedVideo: video })
     }
 
     render() {
         return (
             <div className='ui container'>
                 <SearchBar onTermSubmit={this.onTermSubmit} />
-                I have {this.state.videos.length} videos
-                
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList
                     videos={this.state.videos}
                     onVideoSelect={this.onVideoSelect}
