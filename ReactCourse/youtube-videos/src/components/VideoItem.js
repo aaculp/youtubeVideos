15950803d6ad27ba97c5dfca0db9c97141816ec4
aaculp <aaculp@icloud.com>
2020-03-01 22:14:32 -0500

    added custom styling. Got working api with videos rendering.

--- ReactCourse/youtube-videos/src/components/VideoItem.js
+++ ReactCourse/youtube-videos/src/components/VideoItem.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './VideoItem.css'
 
-const VideoItem = () => {
+const VideoItem = ({ video }) => {
     return (
-        <div>
-            videos
+        <div className='video-item item'>
+            <img
+                className='ui image'
+                alt={video.snippet.description}
+                src={video.snippet.thumbnails.medium.url}
+            />
+            <div className='content'>
+                <div className='header'>
+                    {video.snippet.title}
+                </div>
+            </div>
         </div>
     )
 }
--- ReactCourse/youtube-videos/src/components/VideoList.js
+++ ReactCourse/youtube-videos/src/components/VideoList.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import VideoItem from './VideoItem';
 
-const VideoList = (props) => {
-    const videos = props.videos.map(video => {
+const VideoList = ({ videos }) => {
+    const videoMap = videos.map(video => {
         return <VideoItem key={video.id.videoId} video={video} />
     })
     return (
-        <div>
-            {videos}
+        <div className='ui relaxed divided list'>
+            {videoMap}
         </div>
     )
 }
